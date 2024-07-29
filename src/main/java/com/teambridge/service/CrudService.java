59487dcd08656ffa345bc1353487016e4ef3f12f
DennisSalmazs <skrn.sdet@gmail.com>
2024-07-29 11:19:14 +0100

    services and impls

--- src/main/java/com/teambridge/service/CrudService.java
+++ src/main/java/com/teambridge/service/CrudService.java
@@ -7,8 +7,8 @@ import java.util.List;
 public interface CrudService<T,ID> {
 
     T save(T object);
-    List<T> findAll();
     T findById(ID id);
+    List<T> findAll();
     void deleteById(ID id);
 
 }
--- src/main/java/com/teambridge/service/UserService.java
+++ src/main/java/com/teambridge/service/UserService.java
@@ -2,6 +2,6 @@ package com.teambridge.service;
 
 import com.teambridge.dto.UserDTO;
 
-public interface UserService extends CrudService<UserDTO,String> {
+public interface UserService extends CrudService<UserDTO,String > {
 
 }

4adeeff3fa3b432fdf8c673139f4ef9c1ede1713
DennisSalmazs <skrn.sdet@gmail.com>
2024-08-05 13:51:58 +0100

    list all users and user mapper

--- src/main/java/com/teambridge/service/impl/UserServiceImpl.java
+++ src/main/java/com/teambridge/service/impl/UserServiceImpl.java
@@ -1,17 +1,32 @@
 package com.teambridge.service.impl;
 
 import com.teambridge.dto.UserDTO;
+import com.teambridge.entity.User;
+import com.teambridge.mapper.UserMapper;
+import com.teambridge.repository.UserRepository;
 import com.teambridge.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
-        return List.of();
+        List<User> users = userRepository.findAll();
+        return users.stream().
+                map(userMapper::convertToDTO).
+                collect(Collectors.toList());
     }
 
     @Override

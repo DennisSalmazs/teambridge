03e253a3fac27d66550468d99d9c965996d5d012
DennisSalmazs <skrn.sdet@gmail.com>
2024-08-09 23:49:25 +0100

    request matchers

--- src/main/java/com/teambridge/config/SecurityConfig.java
+++ src/main/java/com/teambridge/config/SecurityConfig.java
@@ -2,12 +2,15 @@ package com.teambridge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,5 +33,21 @@ public class SecurityConfig {
         users.add(user2);
 
         return new InMemoryUserDetailsManager(users);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.
+                authorizeHttpRequests(authorize -> authorize.
+                        requestMatchers(
+                                "/",
+                                "/login",
+                                "/fragments/**",
+                                "/assets/**",
+                                "/images/**"
+                        ).permitAll().
+                        anyRequest().authenticated()).
+                httpBasic(Customizer.withDefaults()).
+                build();
     }
 }

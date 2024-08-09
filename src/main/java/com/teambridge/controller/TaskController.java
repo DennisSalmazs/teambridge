22eb63d282e0f56bc11fb44b64d66782df0d420a
DennisSalmazs <skrn.sdet@gmail.com>
2024-08-09 14:56:08 +0100

    soft delete tasks

--- src/main/java/com/teambridge/controller/TaskController.java
+++ src/main/java/com/teambridge/controller/TaskController.java
@@ -54,12 +54,12 @@ public class TaskController {
 //    public String editTask(@PathVariable Long id, Model model) {
 //
 //        model.addAttribute("task", taskService.findById(id));
-//        model.addAttribute("projects", projectService.findAll());
-//        model.addAttribute("employees", userService.findEmployees());
-//        model.addAttribute("tasks", taskService.findAll());
+//        model.addAttribute("projects", projectService.listAllProjects());
+//        model.addAttribute("employees", userService.listAllByRole("Employee"));
+//        model.addAttribute("tasks", taskService.listAllTasks());
 //        return "task/update";
 //    }
-//
+
 ////    @PostMapping("/update/{taskId}")
 ////    public String updateTask(@PathVariable Long taskId, @ModelAttribute TaskDTO task) {
 ////        // need to set taskId, since it is in DB, but not coming from Task Create form
@@ -84,12 +84,12 @@ public class TaskController {
 //        return "redirect:/task/create";
 //    }
 //
-//    @GetMapping("/delete/{id}")
-//    public String deleteTask(@PathVariable Long id) {
-//
-//        taskService.deleteById(id);
-//        return "redirect:/task/create";
-//    }
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable("id") Long id) {
+
+        taskService.delete(id);
+        return "redirect:/task/create";
+    }
 //
 //    @GetMapping("/employee/pending-tasks")
 //    public String employeePendingTasks(Model model) {

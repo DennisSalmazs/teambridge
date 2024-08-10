6f620e7fbb4f2061a5df251ff98c0f2ca565de0c
DennisSalmazs <skrn.sdet@gmail.com>
2024-08-10 15:19:22 +0100

    authentication success handler

--- src/main/java/com/teambridge/config/SecurityConfig.java
+++ src/main/java/com/teambridge/config/SecurityConfig.java
@@ -3,45 +3,20 @@ package com.teambridge.config;
 import com.teambridge.service.SecurityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     private final SecurityService securityService;
+    private final AuthSuccessHandler authSuccessHandler;
 
-    public SecurityConfig(SecurityService securityService) {
+    public SecurityConfig(SecurityService securityService, AuthSuccessHandler authSuccessHandler) {
         this.securityService = securityService;
+        this.authSuccessHandler = authSuccessHandler;
     }
-
-//    @Bean
-//    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-//
-//        List<UserDetails> users = new ArrayList<>();
-//
-//        User user1 = new User("mike",passwordEncoder.encode("password"),
-//                List.of(new SimpleGrantedAuthority("ROLE_Admin")));
-//
-//        User user2 = new User("ozzy",passwordEncoder.encode("password"),
-//                List.of(new SimpleGrantedAuthority("ROLE_Manager")));
-//
-//        users.add(user1);
-//        users.add(user2);
-//
-//        return new InMemoryUserDetailsManager(users);
-//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -59,10 +34,9 @@ public class SecurityConfig {
                                 "/images/**"
                         ).permitAll()
                         .anyRequest().authenticated())
-//                .httpBasic(Customizer.withDefaults())
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/welcome")
+                        .successHandler(authSuccessHandler)
                         .failureUrl("/login?error=true")
                         .permitAll()
                 )
